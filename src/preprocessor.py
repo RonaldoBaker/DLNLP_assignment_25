57e01f23a528493e758516f997b7f7740a087582
Ronaldo Baker <r.remario.baker@gmail.com>
2025-03-04 09:06:55 +0000

    feat: create new base dataset and remove all punctuation

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -5,6 +5,7 @@ such as file reading, tokenisation, vocabulary creation, etc.
 
 # Import necessary libraries
 import json
+import string
 
 class Preprocessor:
     """
@@ -28,7 +29,8 @@ class Preprocessor:
     
 
     @staticmethod
-    def create_parallel_dictionary(text: list[str], save: bool = False) -> list[dict[str, str]]:
+    def create_parallel_data(text: list[str], format: str, save: bool = False) -> list[dict[str, str]]:
+        # TODO: Come back to this and see if I still need the tuple version so that I can remove it and fix the return type
         """
         Creates a dictionary of parallel sentences between source and target
         languages from a list of strings.
@@ -42,17 +44,29 @@ class Preprocessor:
             sections = line.split("\t")
             eng_sentence, spa_sentence = sections[0], sections[1]
 
+            # Remove punctuation (including inverted question and exclamation marks for Spanish)
+            all_punctuations = string.punctuation + "¡¿"
+            eng_sentence = eng_sentence.translate(str.maketrans("","", all_punctuations))
+            spa_sentence = spa_sentence.translate(str.maketrans("","", all_punctuations))
+
             # Strip the sentences and lowercase them
             eng_sentence = eng_sentence.strip().lower()
             spa_sentence = spa_sentence.strip().lower()
 
             # Add the sentences to the dictionary
-            translations.append({"eng": eng_sentence, "spa": spa_sentence})
+            if format == "dict":
+                translations.append({"eng": eng_sentence, "spa": spa_sentence})
+            elif format == "tuple":
+                translations.append((eng_sentence, spa_sentence))
 
         if save:
-            with open("data/translations.json", "w") as file:
-                json.dump(translations, file, indent=4)
-                file.close()
+            if format == "dict":
+                with open("data/translations_dict.json", "w") as file:
+                    json.dump(translations, file, indent=4)
+                    file.close()
+            elif format == "tuple":
+                with open("data/translations_tuples.json", "w") as file:
+                    json.dump([list(item) for item in translations], file, indent=4)
 
         return translations
     
