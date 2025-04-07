5e594303205633375da24edcfa7e7bd6e1e10966
Ronaldo Baker <r.remario.baker@gmail.com>
2025-04-07 17:30:32 +0100

    feat: multi-tokenisation dataset and collate function

--- src/custom_dataset.py
+++ src/custom_dataset.py
@@ -33,10 +33,38 @@ class TranslationDataset(Dataset):
         eng_tensor = torch.tensor(dictionary['eng_ids'], dtype=torch.long, device=self.device)
         spa_tensor = torch.tensor(dictionary['spa_ids'], dtype=torch.long, device=self.device)
         return eng_tensor, spa_tensor
-    
+
+
+class MultiTokenisationDataset(Dataset):
+    def __init__(self, indexed_dataset, device):
+        self.indexed_data = indexed_dataset
+        self.device = device
+
+    def __len__(self):
+        return len(self.indexed_data)
+
+    def __getitem__(self, idx):
+        dictionary = self.indexed_data[idx]
+        src_dict = {key: torch.tensor(value, dtype=torch.long, device=self.device) for key, value in dictionary.items() 
+                    if key.endswith("_ids") and key.startswith("src_")}
+        tgt_tensor = torch.tensor(dictionary['tgt_word_ids'], dtype=torch.long, device=self.device)
+        return src_dict, tgt_tensor
+
 
 def collate_fn(batch, source_vocab, target_vocab):
     eng_tensors, spa_tensors = zip(*batch)
     eng_tensors = pad_sequence(eng_tensors, padding_value=source_vocab["<pad>"], batch_first=True)
     spa_tensors = pad_sequence(spa_tensors, padding_value=target_vocab["<pad>"], batch_first=True)
     return eng_tensors, spa_tensors
+
+
+def collate_fn_multitokenisation(batch, source_vocab, target_vocab):
+    dicts, tensors = zip(*batch)  # Unpack dictionaries and tensors from the batch
+
+    # Pad the dictionaries
+    padded_dicts = {key: pad_sequence([d[key] for d in dicts], padding_value=source_vocab["<pad>"], batch_first=True) for key in dicts[0].keys()}
+
+    # Pad the individual tensors
+    padded_tensors = pad_sequence(tensors, padding_value=target_vocab["<pad>"], batch_first=True)
+
+    return padded_dicts, padded_tensors
